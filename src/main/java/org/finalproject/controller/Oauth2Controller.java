20e945e4137238bb9f76f8048a05c3357a40ca74
Valeria Vitvytska <leranmu@gmail.com>
2023-09-29 00:46:31 +0300

    Update Oauth2Controller.java

--- src/main/java/org/finalproject/controller/Oauth2Controller.java
+++ src/main/java/org/finalproject/controller/Oauth2Controller.java
@@ -52,8 +52,10 @@ class Oauth2Controller {
         final Claims claims = jwtProvider.getAccessClaims(access);
         final String email = claims.getSubject();
       if (authService.getRefreshStorage().containsKey(email)) {
-           
-
+             Map<String, String> newRefreshStorage = authService.getRefreshStorage();
+                        newRefreshStorage.remove("token");
+                        
+                        authService.setRefreshStorage(newRefreshStorage);   
             return ResponseEntity.ok(new JwtResponse(access,authService.getRefreshStorage().get(email)));
 
         }
